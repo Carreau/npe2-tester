62cab84994f177fd023e8551c7edb3ac459adfb9
Talley Lambert <talley.lambert@gmail.com>
2021-09-01 19:20:40 -0400

    update plugin

--- npe2_tester/_activate.py
+++ npe2_tester/_activate.py
@@ -1,17 +1,11 @@
-from npe2 import register_command
-
-
 def reader(path):
     import tifffile
 
     return [(tifffile.imread(path),)]
 
 
-def napari_get_reader(path):
-    return reader
-
-
 def activate(context):
+    from npe2 import register_command
 
     # one way to do it
     @register_command("my_plugin.hello_world")
@@ -22,4 +16,4 @@ def activate(context):
 
     # another way to do it
     register_command("my_plugin.another_command", lambda: print("yo!"))
-    register_command("my_plugin.my_reader", napari_get_reader)
+    register_command("my_plugin.my_reader", lambda path: reader)
