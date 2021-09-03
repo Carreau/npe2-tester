7ed40f3f6bdf1c8d9d045d9c8ff2a6b5be3bbf31
Talley Lambert <talley.lambert@gmail.com>
2021-09-03 09:36:42 -0400

    add python_name

--- npe2_tester/__init__.py
+++ npe2_tester/__init__.py
@@ -2,3 +2,26 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+
+
+def activate(context):
+    from npe2 import register_command
+
+    # one way to do it
+    @register_command("my_plugin.hello_world")
+    def _hello():
+        from napari.utils.notifications import show_info
+
+        show_info("Hello World!")
+
+    # another way to do it
+    register_command("my_plugin.another_command", lambda: print("yo!"))
+
+
+# this is an example of a function that is registered directly
+# via python_name in the manifest
+def get_reader(path):
+    def reader(path):
+        import tifffile
+
+        return [(tifffile.imread(path),)]
